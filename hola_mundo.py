17e528adf557108745a94f020274fb9467f15188
albertoreal21 <alberto.real@hotmail.com>
2025-11-08 14:27:49 +0100

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+# hola_mundo.py
+# Autor: Alberto Real Sánchez-Serrano
+# Descripción: Programa básico que imprime "Hola Mundo" en pantalla.
+
+print("Hola Mundo")
+
+
+Añadido archivo hola_mundo.py con el programa de Hola Mundo.
